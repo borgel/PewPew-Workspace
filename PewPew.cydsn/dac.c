c28b112c03c954566177ad14b0806b065f845aee
borgel <iborgel@gmail.com>
2017-11-05 15:02:34 -0800

    Fix X/Y DAC swap

--- PewPew.cydsn/dac.c
+++ PewPew.cydsn/dac.c
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 
-#define CONTROL_BITA        (0x5)   //c 0b0101 write to buffer
-#define CONTROL_BITB        (0xC)   //4 0b1100 write to reg a, buffer to b
+#define CONTROL_BITA        (0x5)   //0b0101 write to buffer
+#define CONTROL_BITB        (0xC)   //0b1100 write to reg a, buffer to b
 
 void dac_Init(void){
     SPI_DAC_Start();
@@ -14,11 +14,10 @@ void dac_Init(void){
 
 void dac_WriteSamples(uint16_t x, uint16_t y) {
     uint16_t v;
-    //TODO write A buffer, then B + commit so they go out at the same time
     // mask to 12 bits
-    v = (x & 0xFFF) | (CONTROL_BITA << 12);
+    v = (y & 0xFFF) | (CONTROL_BITA << 12);
     SPI_DAC_Write(v);
     
-    v = (y & 0xFFF) | (CONTROL_BITB << 12);
+    v = (x & 0xFFF) | (CONTROL_BITB << 12);
     SPI_DAC_Write(v);
 }
